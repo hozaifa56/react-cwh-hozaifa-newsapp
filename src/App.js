763efb249b244bf9018cf7a54536b5ce8134e971
HOZAIFA SHAKEEL <hozaifashakeel0@gmail.com>
2023-07-22 04:21:02 +0530

    newsapp navbar news and newsitem created video23

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='container'> hello my first class based component
+      <div className='container'> 
+        <Navbar/>
+        <News/>
         
       </div>
     )
