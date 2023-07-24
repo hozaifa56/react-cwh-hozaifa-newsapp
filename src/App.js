fd3d8543c2c9e5023c2afd86dfcf2502f363aa37
HOZAIFA SHAKEEL <hozaifashakeel0@gmail.com>
2023-07-24 15:21:05 +0530

    video28

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <div className='container'> 
         <Navbar/>
-        <News/>
+        <News pageSize={5}/>
         
       </div>
     )
